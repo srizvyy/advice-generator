da8c0e6f43cc837b330dee6c714c1eb83e29e8fa
Saim Rizvi <shr2mz@gmail.com>
2022-01-25 17:45:24 -0500

    initial commit

---
+++ src/components/AdviceContainer.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+function AdviceContainer() {
+    
+  return (
+  <div>
+
+  </div>
+  )
+}
+
+export default AdviceContainer;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AdviceContainer from './AdviceContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <AdviceContainer/>
+    </div>
+  );
+}
+
+export default App;
